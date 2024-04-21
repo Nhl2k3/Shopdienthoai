15d1d6622ffc43e25171c150dfbebf9866b74d79
Nhl2k3 <kenki28032003@gmail.com>
2024-04-21 21:25:28 +0700

    them hosocanhan_xuly.php

--- Admin/hosocanhan_xuly.php
+++ Admin/hosocanhan_xuly.php
@@ -1,3 +1,28 @@
 <?php
-	echo "<h3>Sinh viên tự xây dựng</h3>";
+   if (session_status() == PHP_SESSION_NONE) {
+    session_start(); // Bắt đầu session nếu chưa được bắt đầu trước đó
+	}
+    require_once("index.php"); // Kết nối CSDL
+	function ThongBaoThanhCong($message) {
+		echo "<div style='color: green;'>$message</div>";
+	}
+    // Lấy thông tin từ FORM
+    $MaNguoiDung = $_POST['MaNguoiDung'];
+    $HoVaTen = $_POST['HoVaTen'];
+
+    // Kiểm tra xem các trường có rỗng không
+    if(trim($HoVaTen) == "") {
+        ThongBaoLoi("Họ và tên không được bỏ trống!");
+    } else {
+        // Cập nhật hồ sơ cá nhân vào CSDL
+        $sql_capnhat = "UPDATE tbl_nguoidung SET TenNguoiDung = '$HoVaTen' WHERE MaNguoiDung = '$MaNguoiDung'";
+        if ($connect->query($sql_capnhat) === TRUE) {
+            ThongBaoThanhCong("Cập nhật hồ sơ thành công!");
+        } else {
+            ThongBaoLoi("Lỗi: " . $sql_capnhat . "<br>" . $connect->error);
+        }
+    }
+
+    // Đóng kết nối CSDL
+    $connect->close();
 ?>
